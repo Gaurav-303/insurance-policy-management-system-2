c362a1173f8c98502f64e2d968a0e521a318d9bf
Gaurav-303 <gaurav134mishra@gmail.com>
2025-09-01 11:37:04 +0530

    add policy

---
+++ src/insurance_system/PolicyManager.java
@@ -0,0 +1,16 @@
+package insurance_system;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class PolicyManager {
+    private Set<Policy> hashSetPolicies = new HashSet<>();
+    private Set<Policy> linkedHashSetPolicies = new LinkedHashSet<>();
+    private Set<Policy> treeSetPolicies = new TreeSet<>();
+
+    public void addPolicy(Policy policy) {
+        hashSetPolicies.add(policy);
+        linkedHashSetPolicies.add(policy);
+        treeSetPolicies.add(policy);
+    }
+}
